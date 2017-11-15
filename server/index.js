396cb7a47114cb8a6f25afec4ba02d3a95f72799
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2017-11-15 22:47:12 +0900

    handle errors in promise

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngr
 const resolve = require('path').resolve;
 const app = express();
 
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
