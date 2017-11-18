a81044f07ad5d2d8c4dfb309bbe8ccdb41aa6380
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2017-11-18 19:29:32 +0900

    pull img instead of push

--- tools/ruby/run.sh
+++ tools/ruby/run.sh
@@ -3,5 +3,4 @@ cd `dirname $0`
 ./run_crawl.sh && \
 ./run_scrape.sh && \
 rm -rf ../../build/img && \
-mv ./data/img ../../build/img && \
 echo `date +%Y%m%d_%H-%M-%S:` 'Finished crawling and scraping.'
