d682c0f97971acbca7addfe136fb4af8c1ee5da4
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2017-11-18 20:27:14 +0900

    print to stderr

--- tools/ruby/scrape.rb
+++ tools/ruby/scrape.rb
@@ -10,7 +10,7 @@ def download(url, savepath)
   open(savepath, 'wb') do |output|
     open(url) do |data|
       output.write(data.read)
-      puts "saved: " + savepath
+      STDERR.puts "saved: " + savepath
     end
   end
 end
